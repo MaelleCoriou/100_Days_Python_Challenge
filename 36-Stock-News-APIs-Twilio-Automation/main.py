1ea3f8d72dc3b1daf46628bbb6b923228ef82130
MAELLE CORIOU <maellecoriou.44@gmail.com>
2021-09-02 19:34:50 +0200

    Updated main stock alert

--- 36-Stock-News-APIs-Twilio-Automation/main.py
+++ 36-Stock-News-APIs-Twilio-Automation/main.py
@@ -4,14 +4,10 @@ from twilio.rest import Client
 from twilio.http.http_client import TwilioHttpClient
 import datetime as dt
 
-STOCK = "TSLA"
-COMPANY_NAME = "Tesla Inc"
+STOCK = "GSAT"
+COMPANY_NAME = "Globalstar Inc"
 
-# Set today and yesterday variables
-today = dt.datetime.now().date()
-yesterday = str(today - dt.timedelta(days=1))
-two_days_ago = str(today - dt.timedelta(days=2))
-
+# --------------------------- TwilioE SET UP --------------------------------#
 # # Proxy client for PythonAnywhere automation
 # proxy_client = TwilioHttpClient()
 # proxy_client.session.proxies = {"https": os.environ["https_proxy"]}
@@ -45,12 +41,18 @@ parameters_stock = {
 request_stock = requests.get("https://www.alphavantage.co/query?", params=parameters_stock)
 data_stock = request_stock.json()["Time Series (Daily)"]
 
+# Set today and yesterday variables
+today = dt.datetime.now().date()
+yesterday = str(today - dt.timedelta(days=1))
+two_days_ago = str(today - dt.timedelta(days=2))
+
 # Get today and yesterday stock values at close time
 yesterday_closing = float(data_stock[yesterday]["4. close"])
-two_days_closing = float(data_stock[two_days_ago]["1. open"])
+two_days_closing = float(data_stock[two_days_ago]["4. close"])
 
-# Stock variation percentage
-variation = round(((two_days_closing - yesterday_closing) / yesterday_closing) * 100)
+# Stock variation
+diff = round(yesterday_closing - two_days_closing)
+variation = (diff / yesterday_closing) * 100
 
 # --------------------------- NEWS SET UP --------------------------------#
 # Get News API key to get latest news info
@@ -58,21 +60,21 @@ key_news = os.environ.get("NEWS_API_KEY")
 
 # 7 days forecast:
 parameters_news = {
-    "q": COMPANY_NAME,
-    "country": "fr",
+    "qInTitle": COMPANY_NAME,
     "sortBy": "popularity",
+    "language": "en",
     "from": yesterday,
     "apiKey": key_news
 }
 
 # Connect with API key
-request_news = requests.get("https://newsapi.org/v2/top-headlines?", params=parameters_news)
-data_news = request_news.json()
+request_news = requests.get("https://newsapi.org/v2/everything?", params=parameters_news)
+data_news = request_news.json()["articles"]
 
 # Get the 3 most popular news
-top_3 = data_news["articles"][0:3]
+top_3 = data_news[0:3]
 
-# Set message function conditions
+# Set message, SMS alert conditions and send message
 text = ""
 if variation >= 5 or variation <= -5:
     for article in top_3:
@@ -91,4 +93,6 @@ if variation >= 5 or variation <= -5:
                     )
         print(message.status)
 
-
+print(yesterday_closing)
+print(two_days_closing)
+print(data_news)
