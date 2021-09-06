aa1bdf3b0a6fce922d68e47237518eb6bf3f289a
MAELLE CORIOU <maellecoriou.44@gmail.com>
2021-09-06 11:58:18 +0200

    Day 38 pushed, API NLP GET & POST for workout session track of calories

---
+++ 38-NLP-API-Nutritionix-WorkoutTrack/main.py
@@ -0,0 +1,69 @@
+import requests
+import os
+from datetime import datetime
+
+# Get Nutritionix + Sheety access codes
+NUTRITION_ID = os.environ.get("NUTRITIONIX_ID")
+NUTRITION_KEY = os.environ.get("NUTRITIONIX_KEY")
+SHEETY_TOKEN = os.environ.get("SHEETY_TOKEN")
+ENDPOINT = os.environ.get("URL_SHEET_ENDPOINT")
+
+# Today's date set up
+TODAY = datetime.now().strftime("%d/%m/%Y")
+TIME = datetime.now().time().strftime("%X")
+
+# ---------------------- NUTRITIONIX POST SET UP -----------------------#
+
+# Authorization headers set up
+header= {
+    "x-app-id": NUTRITION_ID,
+    "x-app-key": NUTRITION_KEY,
+    "Content-Type": "application/json"
+}
+
+# Request fields set up
+request_body = {
+    "query": input("What's your today workout? "),
+    "gender": "female",
+    "weight_kg": 47,
+    "height_cm": 156,
+    "age": 41
+}
+
+# Get Calories regarding request body
+exercise_url = requests.post("https://trackapi.nutritionix.com/v2/natural/exercise",
+                             headers=header,
+                             json=request_body)
+exercise_url.raise_for_status()
+
+data = exercise_url.json()["exercises"][0]
+# print(data)
+
+# ---------------------- SHEETY POST SET UP -----------------------#
+
+header = {
+    "Authorization": f"bearer {SHEETY_TOKEN}"
+}
+
+# New entries based on Nutritionix input and response
+new_row = {
+    "workout": {
+        "date": TODAY,
+        "time": TIME,
+        "exercise": data["name"].title(),
+        "duration": data["duration_min"],
+        "calories": data["nf_calories"]
+    }
+}
+
+# Read current info in spreadsheet
+sheety_url = requests.get(ENDPOINT,
+                          headers=header)
+print(sheety_url.text)
+
+
+# Add new line POST request
+new_line = requests.post(ENDPOINT,
+                         json=new_row,
+                         headers=header)
+print(new_line.text)
